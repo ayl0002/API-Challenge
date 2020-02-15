52ec803c21b2f03f61d200775ca9514d60e12d43
ayl0002 <57423347+ayl0002@users.noreply.github.com>
2020-02-15 11:55:40 -0600

    Add files via upload

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "3f077ddb9aa9123f224f17a738365964"
+weather_api_key = " "
 
 # Google API Key
-g_key = "AIzaSyDdNni4Robho6UOzZIXZwmPPCqK6ZiCh5M"
+g_key = " "
